a2512d60cce691f56abc83430cdc4ef2c810d112
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-13 17:44:47 -0600

    add change color function and event listener

--- js.js
+++ js.js
@@ -1,21 +1,31 @@
-function handleSubmit() {
-    let hexNumbers = [
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-    ];
+let hexNumbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  function changeColor() {
+    let hexColorCode = "";
+    for (let i = 0; i < 6; i++) {
+      let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+      hexColorCode += hexNumbers[randomIndex];
+    }
+    document.body.style.backgroundColor = "#" + hexColorCode;
   }
+
+  document.getElementById("btn").addEventListener("click", changeColor);
+  
   
